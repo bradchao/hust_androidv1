65ead2f96bbf3df1ebfcf55bc8740fddcf316c69
brad <brad@brad.tw>
2017-12-20 17:31:49 +0800

    201712201731

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String hostname =
+            "https://shop.wangzhengyao.idv.tw/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoAccount(View view){
-
+        Intent intent = new Intent(this, WebActivity.class);
+        intent.putExtra("url", hostname +
+                "my-account/edit-account/");
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/user/myapplication/WebActivity.java
+++ app/src/main/java/com/example/user/myapplication/WebActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -8,13 +9,18 @@ import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
     private WebView webView;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
 
+        Intent intent = getIntent();
+        url = intent.getStringExtra("url");
+
         webView = (WebView)findViewById(R.id.webView);
+        initWebView();
 
     }
 
@@ -22,7 +28,8 @@ public class WebActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         WebSettings setting = webView.getSettings();
         setting.setJavaScriptEnabled(true);
-        
+
+        webView.loadUrl(url);
     }
 
 
